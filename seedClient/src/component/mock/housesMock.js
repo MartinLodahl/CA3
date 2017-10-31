5f62246772c4028f3e141266a9fc8831210d33b9
Pravien <pravien@Praviens-MacBook-Pro.local>
2017-10-31 13:54:47 +0100

    s

---
+++ seedClient/src/component/mock/housesMock.js
@@ -0,0 +1,36 @@
+
+const data = [
+    {
+        "id": 1,
+        "address": {},
+        "gps": {},
+        "description": "Super shitty house",
+        "rating": {
+            "stars": 5,
+            "amount": 2
+        },
+        "img": {
+            1: {
+                "url": "https://vignette.wikia.nocookie.net/dunkeypedia/images/4/43/Shitty_house.jpg/revision/latest?cb=20130412201251"
+            },
+            2: {
+                "url": "https://g.rrrather.com/img/q/129969b.jpg"
+            }
+        }
+    },
+    {
+        "id": 2,
+        "address": {},
+        "gps": {},
+        "description": "House",
+        "rating": {
+            "stars": 5,
+            "amount": 2
+        },
+        "img": {
+            1: {
+                "url": "https://static.pexels.com/photos/106399/pexels-photo-106399.jpeg"
+            }
+        }
+    },
+];
