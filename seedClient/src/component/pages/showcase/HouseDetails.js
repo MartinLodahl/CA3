113fe8ba6924bfe59cfc14e649f9142d8c3bdfd9
david carl <david.martin.carl@gmail.com>
2017-10-31 22:37:43 +0100

    more

--- seedClient/src/component/pages/showcase/HouseDetails.js
+++ seedClient/src/component/pages/showcase/HouseDetails.js
@@ -4,18 +4,26 @@ class HouseDetails extends Component {
 
     constructor(props) {
         super();
-        this.state = { "house": props.house}
+        this.state = { "house": props.house }
     }
 
-    componentWillReceiveProps =(nextProps)=>{
-        this.setState({house:nextProps.house});
-   }
+    componentWillReceiveProps = (nextProps) => {
+        this.setState({ house: nextProps.house });
+    }
 
     render() {
         const house = this.state.house;
         return (
             <div>
-                <p>{house.description}</p>
+                <div class="col-sm-4">
+                    <img src={house.img[1].url} width="160" height="160" />
+                </div>
+                <div class="col-sm-5">
+                    <p>{house.description}</p>
+                </div>
+                <div class="col-sm-3">
+                    <p>Average user rating {house.rating.stars} on {house.rating.amount} ratings</p>
+                </div>
             </div>
         )
     }
