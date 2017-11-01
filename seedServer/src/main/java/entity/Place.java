c08b1c3b49c149f648eca9f5a13095755b5861b0
pravien <pravien95@gmail.com>
2017-11-01 10:37:57 +0100

    tilføjelse af title description og rating

--- seedServer/src/main/java/entity/Place.java
+++ seedServer/src/main/java/entity/Place.java
@@ -28,10 +28,19 @@ public class Place implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
+    
+    @OneToOne(cascade=CascadeType.PERSIST)
+    private Rating rating;
+    
     @OneToOne(cascade=CascadeType.PERSIST)
     private Gpslocation gps;
+    @Column(length = 255,name = "_TITLE",nullable = false)
+    private String title;
+    @Column(length = 255,name = "_DESCRIPTION",nullable = false)
+    private String description;
     
-    @OneToOne(mappedBy = "place",cascade=CascadeType.PERSIST)
+    
+    @OneToOne(cascade=CascadeType.PERSIST)
     private Address address;
     
     
@@ -41,13 +50,23 @@ public class Place implements Serializable {
     public Place() {
     }
 
-    public Place(Gpslocation gps, Address address, List<Image> images) {
+    public Place(Rating rating, Gpslocation gps, String title, String description, Address address, List<Image> images) {
+        this.rating = rating;
         this.gps = gps;
+        this.title = title;
+        this.description = description;
         this.address = address;
         this.images = images;
     }
     
-    
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
     public List<Image> getImages() {
         return images;
     }
@@ -81,11 +100,21 @@ public class Place implements Serializable {
         this.gps = gps;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public int hashCode() {
-        int hash = 7;
+        int hash = 5;
         hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.rating);
         hash = 67 * hash + Objects.hashCode(this.gps);
+        hash = 67 * hash + Objects.hashCode(this.description);
         hash = 67 * hash + Objects.hashCode(this.address);
         hash = 67 * hash + Objects.hashCode(this.images);
         return hash;
@@ -103,7 +132,13 @@ public class Place implements Serializable {
             return false;
         }
         final Place other = (Place) obj;
+        if (!Objects.equals(this.description, other.description)) {
+            return false;
+        }
         if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.rating, other.rating)) {
             return false;
         }
         if (!Objects.equals(this.gps, other.gps)) {
@@ -120,8 +155,6 @@ public class Place implements Serializable {
 
     @Override
     public String toString() {
-        return "Place{" + "id=" + id + ", gps=" + gps + ", address=" + address + ", images=" + images + '}';
+        return "Place{" + "id=" + id + ", rating=" + rating + ", gps=" + gps + ", title=" + title + ", description=" + description + ", address=" + address + ", images=" + images + '}';
     }
-    
-    
 }
