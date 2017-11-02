6083b27fe84843dddbdd03ab101e78b1666784c8
pravien <pravien95@gmail.com>
2017-11-02 15:54:51 +0100

    tilføjelser til app

--- ca3-app/src/screen/HomeScreen.js
+++ ca3-app/src/screen/HomeScreen.js
@@ -1,17 +1,121 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, Image } from 'react-native';
+import HuseMock from '../mock/HuseMock';
 
 export default class HomeScreen extends React.Component {
     static navigationOptions = {
       title: 'Rentel',
     };
+
+    
+      
+    
     render() {
+       const doubles = HuseMock.data.map((place)=> {
+        return( <View key={place.id} style={{flex: 1, flexDirection: 'column',justifyContent: 'space-between'}}>
+                <Text style={{textAlign: 'center'}}>{place.id}</Text>
+                <Text style={{textAlign: 'center'}}>{place.title}</Text>
+                <Text style={{textAlign: 'center'}}>{place.description}</Text>
+        </View>);
+      }); 
       const { navigate } = this.props.navigation;
       return (
-       <ScrollView>
-         <View> 
-           
-         </View>
+       <ScrollView> 
+         {doubles}
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
+         <Text>ProfileScreen</Text>
        </ScrollView>
 
       );
@@ -23,3 +127,11 @@ export default class HomeScreen extends React.Component {
           }
         /> */
   }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
